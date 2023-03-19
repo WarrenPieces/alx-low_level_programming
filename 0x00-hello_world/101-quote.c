461acfe4a3729e96d041e0c91216f8f2020b5886
WarrenPieces <christochristoffeldevries@gmail.com>
2023-03-19 16:43:52 -0700

    Quotation mark

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - The main function
+ *
+ * Return: 1 (Success)
+ *
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
