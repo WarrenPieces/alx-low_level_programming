25d8e8a75283eb870de06128d1153312acb02a0f
WarrenPieces <christochristoffeldevries@gmail.com>
2023-04-04 16:52:21 -0700

    Memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fill a space of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: desired value
+ * @n: number of bytes to change
+ * Return: changed array
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (n > 0, i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar = writes the char c to stdout
+ *
+ * @c The charcter to print
+ *
+ * Return: on success 1
+ * 
+ * On error = -1 is returned
+ *
+ */
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
