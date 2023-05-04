beeb482e9f5ec2370c7ebcb6ec6c90171c06d843
Christo de Vries <christochristoffeldevries@gmail.com>
2023-05-03 17:22:08 -0700

    First one to win the race

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void start(void);
+
+/**
+ * start - prints a sentence/input before the coding is executed
+ */
+
+void start(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
